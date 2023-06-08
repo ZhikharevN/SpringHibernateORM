8f58085a31849965258d6f145f48e4a5a17d09de
Zhikharev <zhikharevxx@mail.ru>
2023-06-08 11:14:40 +0300

    Version3.0

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,14 +1,20 @@
 package hiber.model;
 
 import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
 
 @Entity
+@Component
+@Table(name = "cars")
 public class Car {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+    @Column(name = "model")
     private String model;
+    @Column(name = "series")
     private String series;
 
     public Car() {
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,17 +1,22 @@
 package hiber.model;
 
 import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
 
     @OneToOne(cascade = CascadeType.ALL)
@@ -19,6 +24,11 @@ public class User {
     private Car car;
 
     public User() {
+    }
+
+
+    public User(Car car) {
+        this.car = car;
     }
 
     public User(String firstName, String lastName, String email, Car car) {
